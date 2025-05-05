62ea986f722b1618f63f66f41a05332eaca3251f
Paul-antoineLeTolguenec <paul-antoine.le_tolguenec@ensta-bretagne.org>
2025-05-05 08:23:19 +0200

    ray work with agent

--- project/evosim_ray.py
+++ project/evosim_ray.py
@@ -3,6 +3,114 @@ import gymnasium as gym
 import evogym.envs
 import numpy as np
 import time
+from evogym import sample_robot
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import copy
+import matplotlib.pyplot as plt
+from matplotlib import animation
+import gymnasium as gym
+import evogym.envs
+from evogym import sample_robot
+from evogym.utils import get_full_connectivity
+from tqdm import tqdm
+
+
+class Network(nn.Module):
+    def __init__(self, n_in, h_size, n_out):
+        super().__init__()
+        self.fc1 = nn.Linear(n_in, h_size)
+        self.fc2 = nn.Linear(h_size, h_size)
+        self.fc3 = nn.Linear(h_size, n_out)
+ 
+        self.n_out = n_out
+
+    def reset(self):
+        pass
+    
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.relu(x)
+
+        x = self.fc2(x)
+        x = F.relu(x)
+
+        x = self.fc3(x)
+        return x
+
+class Agent:
+    def __init__(self, Net, config, genes = None):
+        self.config = config
+        self.Net = Net
+        self.model = None
+        self.fitness = None
+
+        # self.device = torch.device(
+        #     "cuda" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device("cpu")
+
+        self.make_network()
+        if genes is not None:
+            self.genes = genes
+
+    def __repr__(self):  # pragma: no cover
+        return f"Agent {self.model} > fitness={self.fitness}"
+
+    def __str__(self):  # pragma: no cover
+        return self.__repr__()
+
+    def make_network(self):
+        n_in = self.config["n_in"]
+        h_size = self.config["h_size"]
+        n_out = self.config["n_out"]
+        self.model = self.Net(n_in, h_size, n_out).to(self.device).double()
+        return self
+
+    @property
+    def genes(self):
+        if self.model is None:
+            return None
+        with torch.no_grad():
+            params = self.model.parameters()
+            vec = torch.nn.utils.parameters_to_vector(params)
+        return vec.cpu().double().numpy()
+
+    @genes.setter
+    def genes(self, params):
+        if self.model is None:
+            self.make_network()
+        assert len(params) == len(
+            self.genes), "Genome size does not fit the network size"
+        if np.isnan(params).any():
+            raise
+        a = torch.tensor(params, device=self.device)
+        torch.nn.utils.vector_to_parameters(a, self.model.parameters())
+        self.model = self.model.to(self.device).double()
+        self.fitness = None
+        return self
+
+    def mutate_ga(self):
+        genes = self.genes
+        n = len(genes)
+        f = np.random.choice([False, True], size=n, p=[1/n, 1-1/n])
+        
+        new_genes = np.empty(n)
+        new_genes[f] = genes[f]
+        noise = np.random.randn(n-sum(f))
+        new_genes[~f] = noise
+        return new_genes
+
+    def act(self, obs):
+        # continuous actions
+        with torch.no_grad():
+            x = torch.tensor(obs).double().unsqueeze(0).to(self.device)
+            actions = self.model(x).cpu().detach().numpy()
+        return actions
+
+
+
 
 class EvoGymEnv:
     def __init__(self, env_name, robot):
@@ -33,22 +141,38 @@ class EvoGymEnv:
         obs, reward, done, trunc,  info = self.env.step(action)
         return obs, reward, done, trunc, info
     
+    def close(self):
+        """
+        Close the environment.
+        """
+        self.env.close()
+        # self.env = None
+    
 
 @ray.remote
-def evaluate_env(env, horizon = 1000):
+def evaluate_env(env, agent, horizon = 1000):
     """
     Evaluate the environment for a given number of steps.
     """
-    obs = env.reset()
+    obs, i = env.reset()
     done = False
     value = 0
     for _ in range(horizon):
-        action = env.action_space.sample()
+        action = agent.act(obs)
         obs, reward, done, trunc,  info = env.step(action)
         value += reward
     return value
     
     
+def get_cfg(env_name, robot=None):
+    env = EvoGymEnv(env_name, robot=walker)
+    cfg = {
+        "n_in": env.observation_space.shape[0],
+        "h_size": 32,
+        "n_out": env.action_space.shape[0],
+    }
+    env.close()
+    return cfg
     
     
 walker = np.array([
@@ -61,17 +185,21 @@ walker = np.array([
 env = EvoGymEnv("Walker-v0", walker)
 
 nb_evals = 10
-#  Check time series
-t_0 = time.time()
-for _ in range(nb_evals):
-    task = evaluate_env.remote(env, horizon=1000)
-    result = ray.get(task)
-t_1 = time.time()
-print(f"Time taken for {nb_evals} evaluations in series: {t_1 - t_0:.2f} seconds")
+# #  Check time series
+# t_0 = time.time()
+# for _ in range(nb_evals):
+#     task = evaluate_env.remote(env, horizon=1000)
+#     result = ray.get(task)
+# t_1 = time.time()
+# print(f"Time taken for {nb_evals} evaluations in series: {t_1 - t_0:.2f} seconds")
+
+cfg = get_cfg(env.env_name, robot=walker)
+agent = Agent(Network, cfg)
+
 
 # Check time parallel
 t_0 = time.time()
-tasks = [evaluate_env.remote(env, horizon=1000) for _ in range(nb_evals)]
+tasks = [evaluate_env.remote(env, agent, horizon=1000) for _ in range(nb_evals)]
 results = ray.get(tasks)
 t_1 = time.time()
 print(f"Time taken for {nb_evals} evaluations in parallel: {t_1 - t_0:.2f} seconds")
